f8a417b88cb2e4345474723d3a7c7213662f7977
worldmozara <worldmozara@gmail.com>
2022-01-26 20:55:47 +0800

    prog: 完善模板

--- cyanbot/instance/template.py
+++ cyanbot/instance/template.py
@@ -6,13 +6,35 @@ CORE = {
 # import cyan
 
 from cyan import Session, Ticket
+from cyanbot.runtime import init_runtime
+    """.strip(),
+
+    "CONFIG": """
+app_url = "{0}"
+app_id = "{1}"
+token = "{2}"
     """.strip(),
 
     "SESSION": """
-session = Session(app_url, Ticket("{app_id}", "{token}"))
+session = Session(app_url, Ticket(app_id, token))
     """.strip(),
 
     "RUN": """
-    session.run()
+init_runtime(session)
+session.run()
     """.strip()
+}
+
+# pluginx.py
+PLUGIN = {
+    "IMPORT": """
+# import cyan
+
+from cyanbot.runtime import runtime
+
+if runtime is None:
+    raise Exception("Cyanbot is not running! You should start 'bot.py'.")
+
+session = runtime.session
+    """.strip(),
 }
