732a51f5f0cc7892c6748bb5cd374326d8459145
worldmozara <worldmozara@gmail.com>
2022-01-26 20:20:43 +0800

    prog: 添加 runtime 子模块

---
+++ cyanbot/runtime/__init__.py
@@ -0,0 +1,25 @@
+from cyan import Session
+from .plugin import Plugin
+from typing import Optional
+
+
+class Runtime:
+    """
+    机器人运行环境
+
+    参数:
+        session: 会话对象
+    """
+    def __init__(self, session: Session):
+        self.session = session
+
+    def register_plugin(self, plugin: Plugin):
+        pass
+
+
+runtime: Optional[Runtime] = None
+
+
+def init_runtime(session: Session):
+    global runtime
+    runtime = Runtime(session)
