b3ef72e7d0c953722e0871355bae502362179d37
worldmozara <worldmozara@gmail.com>
2022-01-28 09:56:41 +0800

    prog: 更新模板文件

--- cyanbot/instance/template.py
+++ cyanbot/instance/template.py
@@ -6,7 +6,7 @@ CORE = {
 # import cyan
 
 from cyan import Session, Ticket
-from cyanbot.runtime import init_runtime
+from cyanbot.context import init_context
     """.strip(),
 
     "CONFIG": """
@@ -20,7 +20,10 @@ session = Session(app_url, Ticket(app_id, token))
     """.strip(),
 
     "RUN": """
-init_runtime(session)
+init_context(session)
+
+__import__(".plugins")
+
 session.run()
     """.strip()
 }
@@ -30,17 +33,17 @@ PLUGIN = {
     "IMPORT": """
 # import cyan
 
-from cyanbot.runtime import runtime
+from cyanbot.context import context
 
-if runtime is None:
+if context is None:
     raise Exception("Cyanbot is not running! You should start 'bot.py'.")
 
-session = runtime.session
+session = context.session
     """.strip(),
 
     "INIT": """
 # 该文件用于控制该目录下插件的导入。
-# 你可以自己控制想导入的插件。
+# 你可以以 import 的形式自行控制想导入的插件。
 # 若想导入该目录下的所有插件，可以取消注释下面一行：
 # from . import *
     """.strip()
