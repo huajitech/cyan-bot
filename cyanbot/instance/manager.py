801199636dfddb88a72ce39c93899103867d51aa
worldmozara <worldmozara@gmail.com>
2022-01-30 20:26:34 +0800

    fix: 修复错误

--- cyanbot/instance/manager.py
+++ cyanbot/instance/manager.py
@@ -6,12 +6,16 @@ tree = CommandTree()
 @tree.add_command("help")
 def print_help(tree: CommandTree, _, arg: str) -> None:
     tree.print(
-"""
+        """
 命令:
     help                    输出此帮助信息
     new/create/init         创建新项目实例
 """
     )
 
+
 def plugin(*args: str):
-    tree.run_command(args[0], "")
+    if args:
+        tree.run_command(args[0], "")
+    else:
+        tree.interactive("plugin> ")
