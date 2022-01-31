0662e400d3ac91773e1fa401f3b8b4736148d137
worldmozara <worldmozara@gmail.com>
2022-01-31 09:05:12 +0800

    prog: 完善插件管理

--- cyanbot/instance/manager.py
+++ cyanbot/instance/manager.py
@@ -1,21 +1,52 @@
 from concap import CommandTree
+from os import path, curdir, listdir
+from cyanbot import instance
+import warnings
 
-tree = CommandTree()
+ctree = CommandTree()
 
 
-@tree.add_command("help")
+@ctree.add_command("help")
 def print_help(tree: CommandTree, _, arg: str) -> None:
     tree.print(
         """
 命令:
     help                    输出此帮助信息
-    new/create/init         创建新项目实例
+    new/create/init         创建新插件
 """
     )
 
 
+@ctree.add_command("init")
+@ctree.add_command("create")
+@ctree.add_command("new")
+def new_instance(tree: CommandTree, _, arg: str) -> None:
+    if path.exists("bot.py"):
+        botdir = curdir
+    else:
+        botdir = tree.input("请输入 bot 实例路径：")
+        if path.isfile(botdir):
+            botdir, name = path.split(botdir)
+            if name != "bot.py":
+                raise ValueError("无效路径")
+        else:
+            if "bot.py" not in listdir(botdir):
+                raise ValueError("无效路径")
+    tree.print("正在生成插件......")
+    instance.gen_plugin(path.join(botdir, "plugins"))
+    tree.print("完成！")
+
+
+@ctree.add_command("exit")
+def exit_prog(tree: CommandTree, _, arg: str) -> None:
+    if arg:
+        warnings.warn("调用指令 exit 参数过多。")
+        return
+    tree.run_command("logout", "")
+
+
 def plugin(*args: str):
     if args:
-        tree.run_command(args[0], "")
+        ctree.run_command(args[0], "")
     else:
-        tree.interactive("plugin> ")
+        ctree.interactive("plugin> ")
